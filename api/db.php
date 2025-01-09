cbf060f485c0ff7543b29a4ae341d8426fc6f975
onion <a0966550619@gmail.com>
2025-01-09 10:22:45 +0800

    建立登入表單及驗證登入

---
+++ api/db.php
@@ -0,0 +1,162 @@
+<?php
+
+session_start();
+
+class DB{
+    protected $dsn="mysql:host=localhost;charset=utf8;dbname=web19";
+    protected $pdo;
+    protected $table;
+
+    function __construct($table){
+        $this->table=$table;
+        $this->pdo=new PDO($this->dsn,'root','');
+    }
+
+    /**
+     * 撈出全部資料
+     * 1. 整張資料表
+     * 2. 有條件
+     * 3. 其他SQL功能
+     */
+    function all(...$arg){
+        $sql="SELECT * FROM $this->table ";
+        if(!empty($arg[0])){
+            if(is_array($arg[0])){
+
+                $where=$this->a2s($arg[0]);
+                $sql=$sql . " WHERE ". join(" && ",$where);
+            }else{
+                //$sql=$sql.$arg[0];
+                $sql .= $arg[0];
+
+            }
+        }
+
+        if(!empty($arg[1])){
+            $sql=$sql . $arg[1];
+        }
+
+        return $this->fetchAll($sql);
+    }
+
+    function find($id){
+        $sql="SELECT * FROM $this->table ";
+
+        if(is_array($id)){
+            $where=$this->a2s($id);
+            $sql=$sql . " WHERE ". join(" && ",$where);
+        }else{
+            $sql .= " WHERE `id`='$id' ";
+        }
+        return $this->fetchOne($sql);
+    }
+
+    function save($array){
+
+        if(isset($array['id'])){
+            //update
+            //update table set `欄位1`='值1',`欄位2`='值2' where `id`='值' 
+            $id=$array['id'];
+            unset($array['id']);
+            $set=$this->a2s($array);
+            $sql ="UPDATE $this->table SET ".join(',',$set)." where `id`='$id'";
+                
+        }else{
+            //insert
+            $cols=array_keys($array);
+            $sql="INSERT INTO $this->table (`".join("`,`",$cols)."`) VALUES('".join("','",$array)."')";
+        }
+        
+        //echo $sql;
+        return $this->pdo->exec($sql);
+    }
+    
+    function del($id){
+        $sql="DELETE FROM $this->table ";
+
+        if(is_array($id)){
+            $where=$this->a2s($id);
+            $sql=$sql . " WHERE ". join(" && ",$where);
+        }else{
+            $sql .= " WHERE `id`='$id' ";
+        }
+
+        //echo $sql;  
+        return $this->pdo->exec($sql);
+    }
+
+    
+    /**
+     * 把陣列轉成條件字串陣列
+     */
+    function a2s($array){
+        $tmp=[];
+        foreach($array as $key => $value){
+            $tmp[]="`$key`='$value'";
+        }
+        return $tmp;
+    }
+
+    function max($col,$where=[]){
+        return $this->math('max',$col,$where);
+    }
+    function sum($col,$where=[]){
+        return $this->math('sum',$col,$where);
+    }
+    function min($col,$where=[]){
+        return $this->math('min',$col,$where);
+    }
+    function avg($col,$where=[]){
+        return $this->avg('avg',$col,$where);
+    }
+    function count($where=[]){
+        return $this->math('count','*',$where);
+    }
+
+    /**
+     * 取得單筆資料
+     */
+    protected function fetchOne($sql){
+        //echo $sql;
+        return $this->pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
+    }
+    
+    /**
+     * 取得多筆資料
+     */
+    protected function fetchAll($sql){
+        //echo $sql;
+        return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+    }
+    
+    /**
+     * 方便使用各個聚合函式
+     */
+    
+     protected function math($math,$col='id',$where=[]){
+        $sql="SELECT $math($col) FROM $this->table";
+
+        if(!empty($where)){
+            $tmp=$this->a2s($where);
+            $sql=$sql . " WHERE " . join(" && ", $tmp);
+        }
+
+        return $this->pdo->query($sql)->fetchColumn();
+    }
+
+}
+
+function q($sql){
+    $pdo=new PDO("mysql:host=localhost;charset=utf8;dbname=web19",'root','');
+    return $pdo->query($sql)->fetchAll();
+}
+
+function dd($array){
+    echo "<pre>";
+    print_r($array);
+    echo "</pre>";
+}
+
+function to($url){
+    header("location:".$url);
+}
--- back/main.php
+++ back/main.php
@@ -1,3 +1,42 @@
-<div class="rb tab">
-    <h2 class="ct">請選擇所需功能</h2>
-</div>
+<?php
+
+session_start();
+
+if(isset($_POST['acc'])){
+    if($_POST['acc']=='admin' && $_POST['pw']=='1234'){
+    $_SESSION['login']=1;
+}else{
+    echo "帳號或密碼錯誤";
+}
+}
+if(!isset($_SESSION['login'])){
+    ?>
+<form action="?" method='post'>
+    <table class="tab" style="width: 300px;">
+        <tr>
+            <td>帳號：</td>
+            <td>
+                <input type="text" name="acc" id="acc">
+            </td>
+        </tr>
+        <tr>
+            <td>密碼：</td>
+            <td>
+                <input type="password" name="pw" id="pw">
+            </td>
+        </tr>
+    </table>
+    <div class="ct">
+        <input type="submit" value="登入">
+        <input type="reset" value="重置">
+    </div>
+    <?php
+}else{
+?>
+    <div class="rb tab">
+        <h2 class="ct">請選擇所需功能</h2>
+    </div>
+    <?php    
+}
+?>
+</form>

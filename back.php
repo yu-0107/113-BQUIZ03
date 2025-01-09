1112bc3cff508787d6b99e141f20ab1856ac54bb
onion <a0966550619@gmail.com>
2025-01-09 10:56:10 +0800

    建立預告片後台管理頁面表單

--- back.php
+++ back.php
@@ -1,4 +1,4 @@
-<?php include_once "api/db.php" ;?>
+<?php include_once "api/db.php"; ?>
 <!DOCTYPE html
     PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <!-- saved from url=(0055)?do=admin -->
@@ -15,47 +15,47 @@
 <body>
     <div id="main">
         <div id="top" style=" background:#999 center; background-size:cover; " title="替代文字">
-            <h1>ABC影城</h1>
+            <h1 style="margin:0;padding:0">ABC影城</h1>
         </div>
         <div id="top2">
-            <div id="top2">
-                <a href="index.htm">首頁</a>
-                <a href="order.htm">線上訂票</a>
-                <a href="#">會員系統</a>
-                <a href="back.htm">管理系統</a>
-            </div>
-            <div id="text"> <span class="ct">最新活動</span>
-                <marquee direction="right">
-                    ABC影城票價全面八折優惠1個月
-                </marquee>
-            </div>
-            <div id="mm">
-                <div class="ct a rb" style="position:relative; width:101.5%; left:-1%; padding:3px; top:-9px;">
-                    <a href="?do=tit">網站標題管理</a>|
-                    <a href="?do=go">動態文字管理</a>|
-                    <a href="?do=poster">預告片海報管理</a>|
-                    <a href="?do=movie">院線片管理</a>|
-                    <a href="?do=order">電影訂票管理</a>
-                </div>
-
-                <?php 
-
-            $do=$_GET['do']??'main';
-            $file="./back/$do.php";
-
-            if(file_exists($file)){
-              include $file;
-            }else{
-              include "./back/main.php";
-            }
-
-
-
-            ?>
-
-            </div>
-            <div id="bo"> ©Copyright 2010~2014 ABC影城 版權所有 </div>
+            <a href="index.php">首頁</a>
+            <a href="order.php">線上訂票</a>
+            <a href="#">會員系統</a>
+            <a href="back.php">管理系統</a>
         </div>
+        <div id="text"> <span class="ct">最新活動</span>
+            <marquee direction="right">
+                ABC影城票價全面八折優惠1個月
+            </marquee>
+        </div>
+        <div id="mm">
+            <div class="ct a rb" style="position:relative; width:101.5%; left:-1%; padding:3px; top:-9px;">
+                <a href="?do=tit">網站標題管理</a>|
+                <a href="?do=go">動態文字管理</a>|
+                <a href="?do=poster">預告片海報管理</a>|
+                <a href="?do=movie">院線片管理</a>|
+                <a href="?do=order">電影訂票管理</a>
+            </div>
+
+            <?php 
+  
+  $do=$_GET['do']??'main';
+  $file="./back/$do.php";
+
+  if(file_exists($file)){
+    include $file;
+  }else{
+    include "./back/main.php";
+  }
+
+  
+
+?>
+
+
+        </div>
+        <div id="bo"> ©Copyright 2010~2014 ABC影城 版權所有 </div>
+    </div>
 </body>
 
 </html>
--- back/poster.php
+++ back/poster.php
@@ -1,1 +1,21 @@
-sadsd
+<div style="height: 340px;">
+    <h3 class="ct">預告片清單</h3>
+</div>
+<hr>
+<div style="height: 170px;">
+    <h3 class="ct">新增預告片海報</h3>
+    <form action="./api/add_poster.php" method="post" enctype="multipart/form-data">
+        <table>
+            <tr>
+                <td>預告片海報：</td>
+                <td><input type="file" name="img" id=""></td>
+                <td>預告片片名：</td>
+                <td><input type="text" name="name" id=""></td>
+            </tr>
+        </table>
+        <div class="ct">
+            <input type="submit" value="新增">
+            <input type="reset" value="重置">
+        </div>
+    </form>
+</div>
